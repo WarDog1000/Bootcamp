ba386a06e26eac3545b80d7f7c843cc6a81811e8
Jhony Souza <jhonatansouzameza100@gmail.com>
2022-11-09 20:03:38 -0300

    FT-M1 01-homework

--- FT-M1/testeos/testeos.js
+++ FT-M1/testeos/testeos.js
@@ -1,14 +1,12 @@
-'use strict'
-
 function testA(num) {
   // tu codigo aca
-
+  
 }
 
 function testB(num) {
   // tu codigo aca
-
+  
 }
 
-console.log(testA());
-// console.log(testB());
+console.log(testA(110));
+console.log(testB(6));
